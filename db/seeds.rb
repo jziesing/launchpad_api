5576519b8bf0dd58ee10e877e03622e63cb400a1
Ryan Francis <ryan.p.francis@gmail.com>
2020-08-28 14:51:40 -0400

    update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 # seeds file runs every deployment since it is a task in the release
 # I couldn't figure out a way to trigger a rake task via Heroku Platform API other than using the release mechanism
-if AdminUser.count == 0
+if AdminUser.count == 0 # only run once
   AdminUser.create(email: 'admin@example.com', password: 'password')
 end
